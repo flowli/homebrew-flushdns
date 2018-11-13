94443f5f68ad88b5d8cd9985157836f6330b803c
Florian Arndt <post@florianarndt.com>
2018-11-13 15:01:45 +0100

    WiP: flushdns tap

--- Formula/flushdns.rb
+++ Formula/flushdns.rb
@@ -1,9 +1,9 @@
 class FlushDNS < Formula
   desc "Flushes DNS cache on macOS with command flushdns"
   homepage "https://arweb.de"
-  url "https://github.com/flowli/homebrew-flushdns/???", :using => :curl
+  url "https://github.com/flowli/homebrew-flushdns/raw/master/flushdns/flushdns.zip", :using => :curl
 
   def install
-     bin.install ""
+     bin.install "flushdns"
   end
 end
