7464fd79a0a86d0af6c5a5333d34d0e8498fcaaf
Florian Arndt <post@florianarndt.com>
2018-11-13 15:03:04 +0100

    class name fixed

--- Formula/flushdns.rb
+++ Formula/flushdns.rb
@@ -1,4 +1,4 @@
-class FlushDNS < Formula
+class Flushdns < Formula
   desc "Flushes DNS cache on macOS with command flushdns"
   homepage "https://arweb.de"
   url "https://github.com/flowli/homebrew-flushdns/raw/master/flushdns/flushdns.zip", :using => :curl
