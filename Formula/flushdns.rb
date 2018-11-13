1379705b3419372e93d24f3493bd2feae90be2de
Florian Arndt <post@florianarndt.com>
2018-11-13 15:06:08 +0100

    version added

--- Formula/flushdns.rb
+++ Formula/flushdns.rb
@@ -1,6 +1,7 @@
 class Flushdns < Formula
   desc "Flushes DNS cache on macOS with command flushdns"
   homepage "https://arweb.de"
+  version "0.1"
   url "https://github.com/flowli/homebrew-flushdns/raw/master/flushdns/flushdns.zip", :using => :curl
 
   def install
